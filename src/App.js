7dc7ebbfb9b20fa82225ae59a23a69d837f4e683
rodrigoc01 <rodrigocamargo1234santos@gmail.com>
2023-10-22 09:51:33 -0300

    adicionado botão de deletar colaborador

--- src/App.js
+++ src/App.js
@@ -193,6 +193,9 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
+  function deletarColaborador() {
+    console.log('Deletando colaborador');
+  } //essa função será passada para o componente Time, que por sua vez passará para o componente Colaborador
 
   return (
     <div>
@@ -200,7 +203,15 @@ function App() {
       <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
       <section className="times">
         <h1>Minha organização</h1>
-        {times.map((time, indice) => <Time key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} />)}
+        {times.map((time, indice) => 
+          <Time 
+            key={indice} 
+            time={time} 
+            colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} 
+            // passando função deletarColaborador para o componente Time
+            aoDeletar={deletarColaborador}
+          />
+        )}
       </section>
       <Rodape />
     </div>
--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,14 @@
+// importando ícone
+//precisa de chaves pois não é um export default
+import {AiFillCloseCircle} from "react-icons/ai";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+// cada div colaborador vai receber um colaborador e uma cor de fundo como propriedade 
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
+        {/* componente para deletar colaborador */}
+        <AiFillCloseCircle size={28} className='deletar' onClick={aoDeletar}/>
+
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
@@ -12,4 +19,4 @@ const Colaborador = ({ colaborador, corDeFundo }) => {
     </div>)
 }
 
-export default Colaborador
+export default Colaborador //usando export default pois só tem um componente por arquivo
